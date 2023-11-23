fa11d9a8ca2925ccdefc35aa3277f18d6d3219fa
CK-Arijit <arijit.naskar@cloudkaptan.com>
2023-11-23 16:03:49 +0530

    LWC components

--- LWC/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWC/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    //properties
+    fullName = "Zero to Hero"
+    @track title = "Aura"
+
+    changeHandler(event) {
+        this.title = event.target.value;
+    }
+
+    address = {
+        city: 'Melbourne',
+        postCode: 3008,
+        country: 'Australia'
+    }
+
+    trackHandler(event) {
+        this.address = {...this.address, "city": event.target.value} //Spread operator
+    }
+
+    users = ["John", "Smith", "Ram"]
+    num1 = 23
+    num2= 3
+
+    get firstUser() {
+        return this.users[0]
+    }
+
+    get multiply() {
+        return this.num1*this.num2
+    }
+}
